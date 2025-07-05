de4c4ebe2bced8f756a191ab728883b769029832
snailsquid <hexagononyt@gmail.com>
2025-07-06 03:50:09 +0700

    fix: foodInstance null check

--- Assets/Scripts/Mechanic/Itsuki/TsundereAttack.cs
+++ Assets/Scripts/Mechanic/Itsuki/TsundereAttack.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Mechanic.Itsuki
@@ -9,19 +10,28 @@ namespace Mechanic.Itsuki
     private bool _isLaunched = false;
     private float _velocity;
     private Vector3 _direction;
+    private float _lifetime;
     private void Start()
     {
       _sr = GetComponent<SpriteRenderer>();
     }
 
-    public void Initialize(Sprite sprite)
+    public void Initialize(Sprite sprite, float lifetime)
     {
       if (_sr == null)
       {
         _sr = GetComponent<SpriteRenderer>();
       }
       _sr.sprite = sprite;
+      _lifetime = lifetime;
+      StartCoroutine(DestroyCoroutine());
     }
+
+    private IEnumerator DestroyCoroutine()
+    {
+      yield return new WaitForSeconds(_lifetime);
+      Destroy(this.gameObject);
+    } 
 
     public void Launch(Vector3 direction, float velocity)
     {
@@ -32,7 +42,15 @@ namespace Mechanic.Itsuki
 
     private void FixedUpdate()
     {
-      transform.position += _direction * (_velocity * Time.fixedDeltaTime);
+      if(_isLaunched)
+        transform.position += _direction * (_velocity * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+      var health = other.GetComponent<Health>();
+      if (health == null) return;
+      health.Damage();
     }
   }
 }
