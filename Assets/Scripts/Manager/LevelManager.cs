ac3282f881358c295fd8f12903b0604198177a67
Raysha Erviandika Putra <19624013@mahasiswa.itb.ac.id>
2025-07-08 11:32:37 +0700

    feat: LevelManager

---
+++ Assets/Scripts/Manager/LevelManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public enum SceneID
+    {
+        MainMenu,
+        LevelMiku,
+        LevelYotsuba,
+        LevelItsuka,
+        LevelIchika,
+        LevelNino,
+        Ending
+    }
+
+    [Serializable]
+    struct SceneMapping
+    {
+        public SceneID id;
+        public string sceneName; 
+    }
+
+    [SerializeField] private SceneMapping[] s_sceneMapping;
+
+    public static LevelManager Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public static void ChangeLevel(SceneID scene)
+    {
+        if (Instance == null) return;
+        for (int i = 0; i < Instance.s_sceneMapping.Length; i++)
+        {
+            var sceneValue = Instance.s_sceneMapping[i];
+            if (sceneValue.id == scene)
+            {
+                SceneManager.LoadScene(sceneValue.sceneName);
+                return;
+            }
+        }
+    }
+}
