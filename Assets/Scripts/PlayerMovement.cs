95ef9acfb574966a25d5fa8fb32b85346969227a
Raysha Erviandika Putra <19624013@mahasiswa.itb.ac.id>
2025-07-10 14:15:35 +0700

    Merge branch 'MergeTest' of https://github.com/AzriVz/SPARTA2024_GameDevelopment into MergeTest

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
+
+
+
 
 public class movement : MonoBehaviour
 {
@@ -11,11 +15,18 @@ public class movement : MonoBehaviour
     [SerializeField] public LayerMask groundLayer;
 
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private AnimatorOverrideController overrideController;
     private bool isGrounded;
+    private Animator _animator;
+    private SpriteRenderer _sr;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _animator.runtimeAnimatorController = overrideController;
+        
+        _sr = GetComponent<SpriteRenderer>();
     }
 
     void Update()
@@ -26,10 +37,31 @@ public class movement : MonoBehaviour
         // Player movement
         float moveInput = Input.GetAxisRaw("Horizontal");
         rb.linearVelocity = new Vector2(moveInput * speed, rb.linearVelocity.y);
+        if (moveInput > 0)
+        {
+            _animator.SetBool("isRunning", true);
+            _sr.flipX = true;
+            
+        }else if (moveInput < 0)
+        {
+            _animator.SetBool("isRunning", true);
+            _sr.flipX = false;
+        }else if (moveInput == 0)
+        {
+            _animator.SetBool("isRunning", false);
+        }
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded && Mathf.Abs(rb.linearVelocity.y) < 0.01f)
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jump);
+        }
+
+        if (isGrounded)
+        {
+            _animator.SetBool("isJumping", false);
+        }else
+        {
+            _animator.SetBool("isJumping", true);
         }
     }
 
